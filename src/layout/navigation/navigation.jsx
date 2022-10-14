8dbce050adb953b9b02dbcfb4f7e569e377ec865
KassieSanisclaws <86675041+KassieSanisclaws@users.noreply.github.com>
2022-10-14 02:29:37 -0400

    nineth commit

--- src/layout/navigation/navigation.jsx
+++ src/layout/navigation/navigation.jsx
@@ -3,6 +3,7 @@ import "./navigation.css";
 import QuizLogo from "../../images/iconImages/site-icon2.png";
 import TestIcon from "../../images/iconImages/testIcon.png";
 import PricingIcon from "../../images/iconImages/priceIcon.png";
+import HomeIcon from "../../images/iconImages/homeIcon.png";
 
 export default function Navigation(){
     return (
@@ -11,6 +12,7 @@ export default function Navigation(){
                <img src={QuizLogo} alt="quiz-logo"/>
            </Link>
              <ul>
+                <li><Link to="/"><img src={HomeIcon} alt="homeIcon"/><span>Home</span></Link></li>
                 <li><Link to="/tests"><img src={TestIcon} alt="testIcon"/><span>Tests</span></Link></li>      
                 <li> <Link to="/pricing"><img src={PricingIcon} alt="pricingIcon"/><span>Pricing</span></Link></li>               
              </ul>
